30a70f8886cd67f81facd1b713f7ebfb9f413034
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-01-23 12:38:12 -0500

    - small change - web-vitals readded

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just a test. If you see this in https://pedrodominguezutec.github.io/test/ then it is working.
         </p>
         <a
           className="App-link"
